688e75883c363ea2a5974b8311c20143fb4c51d9
Antony Alkmim <antony-alkmim@hotmail.com>
2017-06-04 19:18:10 -0300

    list registered rooms

--- app/controllers/roomController.js
+++ app/controllers/roomController.js
@@ -5,14 +5,7 @@ const Room = require("../models/room");
 //Get Rooms - List rooms
 //========================================
 exports.listRooms = (req,res) => {
-    // Room.find({})
-    //     .then(docs => res.json(docs))
-    //     .catch(err => res.sendStatus(400));
-    res.json([
-		{_id: "123", title : 'Cozinha', description: 'Cozinha', type: 'kitchen', isLightOn : true },
-		{_id: "123", title : 'Sala de Estar', description: 'Sala de estar', type: 'livingRoom', isLightOn : false },
-		{_id: "123", title : 'Garagem', description: 'Garagem', type: 'garage', isLightOn : true },
-		{_id: "123", title : 'Quarto 1', description: 'Quarto das crianças', type: 'bedRoom', isLightOn : false },
-		{_id: "123", title : 'Quarto 2', description: 'Quarto das crianças', type: 'bedRoom', isLightOn : true }
-    	]);
+    Room.find({})
+        .then(docs => res.json(docs))
+        .catch(err => res.sendStatus(400));
 };
